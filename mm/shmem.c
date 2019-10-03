2d9e7df16e799bbb0b9ff72ea2a8c88abc057aea
Arne Coucheron <arco68@gmail.com>
2019-10-03 04:04:47 +0200

    Revert "shmem: fix splicing from a hole while it's punched"
    This reverts commit 21618a8f0f46ff327acd6a079bc05c065561e4c0.
    
    Change-Id: I1da9a6dbb1cb829c3964c3c29210872920a15898

--- mm/shmem.c
+++ mm/shmem.c
@@ -519,18 +519,21 @@ void shmem_truncate_range(struct inode *inode, loff_t lstart, loff_t lend)
 		return;
 
 	index = start;
-	while (index <= end) {
+	for ( ; ; ) {
 		cond_resched();
 		pvec.nr = shmem_find_get_pages_and_swap(mapping, index,
 				min(end - index, (pgoff_t)PAGEVEC_SIZE),
 							pvec.pages, indices);
 		if (!pvec.nr) {
-			/* If all gone or hole-punch, we're done */
-			if (index == start || end != -1)
+			if (index == start)
 				break;
-			/* But if truncating, restart to make sure all gone */
 			index = start;
 			continue;
+		}
+		if (index == start && indices[0] > end) {
+			shmem_deswap_pagevec(&pvec);
+			pagevec_release(&pvec);
+			break;
 		}
 		mem_cgroup_uncharge_start();
 		for (i = 0; i < pagevec_count(&pvec); i++) {
@@ -541,12 +544,8 @@ void shmem_truncate_range(struct inode *inode, loff_t lstart, loff_t lend)
 				break;
 
 			if (radix_tree_exceptional_entry(page)) {
-				if (shmem_free_swap(mapping, index, page)) {
-					/* Swap was replaced by page: retry */
-					index--;
-					break;
-				}
-				nr_swaps_freed++;
+				nr_swaps_freed += !shmem_free_swap(mapping,
+								index, page);
 				continue;
 			}
 
@@ -554,11 +553,6 @@ void shmem_truncate_range(struct inode *inode, loff_t lstart, loff_t lend)
 			if (page->mapping == mapping) {
 				VM_BUG_ON(PageWriteback(page));
 				truncate_inode_page(mapping, page);
-			} else {
-				/* Page was replaced by swap: retry */
-				unlock_page(page);
-				index--;
-				break;
 			}
 			unlock_page(page);
 		}
